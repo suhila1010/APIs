9d510375c3e443579f47556b443797803ead53f4
suhila1010 <sohilaahmed678@gmail.com>
2025-08-08 23:26:24 +0300

    feat: add user validation and authentication to GraphQL resolvers

--- app.js
+++ app.js
@@ -41,13 +41,30 @@ app.use((req, res, next) =>{
     res.setHeader('Access-Control-Allow-Origin','*')
     res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE')
     res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization')
+    if(req.method === "OPTIONS"){
+        return res.sendStatus(200)
+    }
     next()
 })
 
 app.use('/graphql' , graphqlHTTP({
     schema: graphqlSchema,
     rootValue:graphqlResolver,
-    graphiql: true
+    graphiql: true,
+    formatError(err){
+        if(!err.originalError){
+            return err
+        }
+        const data = err.originalError.data
+        const message = err.message || 'An error occurred.'
+        const code = err.originalError.code || 500
+
+        return{
+            message:message,
+            status:code,
+            data:data
+        }
+    }
 }))
 
 app.use((error, req, res, next) => {
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,28 @@
 const bcrypt = require('bcryptjs')
 const User = require('../models/user')
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
 
 module.exports = {
     createUser: async function ({userInput}, req) {
         // const email = userInput.email
+        const errors = []
 
+        if(!validator.isEmail(userInput.email)){
+            errors.push({message:"Email is not valid"})
+        }
+
+        if(validator.isEmpty(userInput.password) || !validator.isLength(userInput.password,{min:5} )){
+            errors.push({message:"Password is not valid"})
+        }
+
+        if(errors.length > 0) {
+            const error = new Error('Invalid Input')
+            error.data = errors
+            error.code = 422
+            throw error
+        }
+        console.log(errors[0])
         const existingUser = await User.findOne({email: userInput.email})
         if(existingUser){
             const error = new Error('User exits already!')
@@ -20,5 +38,27 @@ module.exports = {
         const createdUser = await user.save()
 
         return {...createdUser, _id: createdUser._id.toString()}
+    },
+
+    login:async function({email, password}){
+        const user = await User.findOne({email:email})
+        if(!user){
+            const error = new Error('User Not Found!')
+            error.code = 401
+            throw error
+        }
+        const isEqual = await bcrypt.compare(password, user.password)
+        if(!isEqual){
+            const error = new Error('Password is incorrect.')
+            error.code = 401
+            throw error
+        }
+        const token = jwt.sign({
+            userId: user._id.toString(),
+            email:user.email
+        }, 'somesupersecretsecret', {expiresIn: '1h'})
+
+        return {token:token, userId:user._id.toString()}
     }
+
 }
